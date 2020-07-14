8913814ce6945de85301f7bf0a3b58571cfa8b35
STEdirisuriya <supunedirisuriya2002@gmail.com>
2020-07-14 16:40:07 +0530

    added stats

---
+++ js/chartsjs.js
@@ -0,0 +1,66 @@
+var ctx = document.getElementById('alltasks').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['2018', '2019', '2020'],
+        datasets: [{
+            label: '# of Tasks',
+            data: [55, 100, 890],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.4)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: false
+                }
+            }]
+        }
+    }
+});
+
+var ctx2 = document.getElementById('allcatogery').getContext('2d');
+var myChart2 = new Chart(ctx2, {
+    type: 'bar',
+    data: {
+        labels: ['2018', '2019', '2020'],
+        datasets: [{
+            label: 'Code Tasks',
+            data: [50, 100, 65],
+            backgroundColor:  'rgba(153, 102, 255, 0.4)',
+            borderColor: 'rgba(153, 102, 255, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Design Tasks',
+            data: [50, 05, 99],
+            backgroundColor: 'rgba(255, 159, 64, 0.4)',
+            borderColor: 'rgba(255, 159, 64, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Explore Tasks',
+            data: [50, 65, 65],
+            backgroundColor:  'rgba(75, 192, 192, 0.4)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+        }
+        ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: false
+                }
+            }]
+        }
+    }
+});
